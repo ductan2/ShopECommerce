11e9cc143c5d5e2f001f172c4844a9c8527a5863
root <root@DESKTOP-6JPDVLR.localdomain>
2023-09-30 15:26:06 +0700

    upload v2

--- index.ts
+++ index.ts
@@ -1,3 +1,25 @@
-import app from './src/index';
+import express, { NextFunction, Request, Response } from 'express';
+import { config } from 'dotenv';
+import databaseServices from './src/services/database.services';
+import router from './src/routes/index.routes';
+import cookieParser from 'cookie-parser';
+import cors from "cors"
 
+config();
+const app = express();
+const PORT = process.env.PORT;
+app.use(cors({ origin: true, credentials: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser())
+// app.use(cors)
+databaseServices.connect();
+app.listen(PORT, () => {
+  console.log(`This is http://localhost:${PORT}`)
+})
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.status).json({ error: err.message, status: err.status })
+})
+app.use('/api', router) 
 export default app;
